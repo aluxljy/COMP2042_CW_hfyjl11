426772e2cb732f7c300931cfbec73cb70e44c28b
aluxljy <aluxljy@gmail.com>
2021-12-09 01:41:24 +0800

    added comments

--- src/test/BrickFactory.java
+++ src/test/BrickFactory.java
@@ -6,6 +6,7 @@ public class BrickFactory {
     /**
      * called in Level
      */
+    // use method makeBrick to get object of type Brick
     public Brick makeBrick(String brickType, Point point, Dimension size) {
         if(brickType == null) {
             return null;
