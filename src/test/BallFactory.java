745469a44db9ca7fcbe0558a1d2b7ad4537b842a
aluxljy <aluxljy@gmail.com>
2021-12-09 02:59:49 +0800

    added comments

--- src/test/BallFactory.java
+++ src/test/BallFactory.java
@@ -3,6 +3,10 @@ package test;
 import java.awt.geom.Point2D;
 
 public class BallFactory {
+    /**
+     * called in Wall
+     */
+    // use method makeBall to get object of type Ball
     public Ball makeBall(String ballType,Point2D center) {
         if(ballType == null) {
             return null;
