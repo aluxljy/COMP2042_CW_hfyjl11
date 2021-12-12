9e8281b69a5cfe46ce4d9cad8178d344e64cf069
aluxljy <aluxljy@gmail.com>
2021-12-12 18:54:26 +0800

    removed unused comments and imports, added javadocs for Level class

--- src/BrickDestroyer/Model/Level.java
+++ src/BrickDestroyer/Model/Level.java
@@ -1,58 +1,21 @@
 package BrickDestroyer.Model;
 
-import BrickDestroyer.Model.Brick;
-import BrickDestroyer.Model.BrickFactory;
-
 import java.awt.*;
 
+/**
+ * to arrange different bricks together into different combinations that can be used to make new levels
+ */
 public class Level {
-    /*private Brick[][] levels;
-    private int level;
-    private int brickCount;*/
-
-    /*public static Brick[] makeSingleTypeLevel(Rectangle drawArea, int brickCount, int lineCount, double brickSizeRatio, String type){
-        BrickFactory brickFactory = new BrickFactory();
-        *//*
-          if brickCount is not divisible by line count,brickCount is adjusted to the biggest
-          multiple of lineCount smaller then brickCount
-         *//*
-        brickCount -= brickCount % lineCount;
-
-        int brickOnLine = brickCount / lineCount;
-
-        double brickLength = drawArea.getWidth() / brickOnLine;
-        double brickHeight = brickLength / brickSizeRatio;
-
-        brickCount += lineCount / 2;
-
-        Brick[] tmp  = new Brick[brickCount];
-
-        Dimension brickSize = new Dimension((int) brickLength,(int) brickHeight);
-        Point p = new Point();
-
-        int i;
-        for(i = 0; i < tmp.length; i++){
-            int line = i / brickOnLine;
-            if(line == lineCount)
-                break;
-            double x = (i % brickOnLine) * brickLength;
-            x =(line % 2 == 0) ? x : (x - (brickLength / 2));
-            double y = (line) * brickHeight;
-            p.setLocation(x,y);
-            //tmp[i] = Wall.makeBrick(p,brickSize,type);
-            tmp[i] = brickFactory.makeBrick(type,p,brickSize);
-        }
-
-        for(double y = brickHeight;i < tmp.length;i++, y += 2*brickHeight){
-            double x = (brickOnLine * brickLength) - (brickLength / 2);
-            p.setLocation(x,y);
-            //tmp[i] = Wall.makeBrick(p,brickSize,type);
-            tmp[i] = brickFactory.makeBrick(type,p,brickSize);
-        }
-        return tmp;
-
-    }*/
-
+    /**
+     * called in Wall, to arrange 2 types of bricks into a chessboard pattern
+     * @param drawArea area of the rectangular shape
+     * @param brickCount number of bricks
+     * @param lineCount number of lines
+     * @param brickSizeRatio ratio of the brick size
+     * @param typeA name of brickA
+     * @param typeB name of brickB
+     * @return a list of bricks
+     */
     public static Brick[] makeChessboardLevel(Rectangle drawArea, int brickCount, int lineCount, double brickSizeRatio, String typeA, String typeB) {
         BrickFactory brickFactory = new BrickFactory();
         /*
@@ -88,24 +51,14 @@ public class Level {
             point.setLocation(x,y);
 
             boolean brick = ((line % 2 == 0 && i % 2 == 0) || (line % 2 != 0 && positionX > centerLeft && positionX <= centerRight));
-            //tmp[i] = brick ?  Wall.makeBrick(point,brickSize,typeA) : Wall.makeBrick(point,brickSize,typeB);
             tmp[i] = brick ? brickFactory.makeBrick(typeA,point,brickSize) : brickFactory.makeBrick(typeB,point,brickSize);
         }
 
         for(double y = brickHeight;i < tmp.length;i++, y += 2*brickHeight){
             double x = (brickOnLine * brickLength) - (brickLength / 2);
             point.setLocation(x,y);
-            //tmp[i] = Wall.makeBrick(point,brickSize,typeA);
             tmp[i] = brickFactory.makeBrick(typeA,point,brickSize);
         }
         return tmp;
     }
-   /* public void nextLevel(){
-        Wall.setBricks(levels[level++]);
-        this.brickCount = Wall.getBricks().length;
-    }
-
-    public boolean hasLevel(){
-        return level < levels.length;
-    }*/
 }
