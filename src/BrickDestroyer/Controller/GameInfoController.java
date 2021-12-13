f961b6b900b35a84d9f3b6b0c60443140bd2870c
aluxljy <aluxljy@gmail.com>
2021-12-13 15:19:51 +0800

    added javadocs and completed GameInfo and GameInfoController class

--- src/BrickDestroyer/Controller/GameInfoController.java
+++ src/BrickDestroyer/Controller/GameInfoController.java
@@ -6,16 +6,24 @@ import BrickDestroyer.View.GameInfo;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Controller for GameInfo
+ */
 public class GameInfoController implements ActionListener {
     private GameInfo gameInfo;
 
     /**
-     * called in GameInfo
+     * called in GameInfo, GameInfoController constructor
+     * @param gameInfo current gameInfo
      */
-    // GameInfoController constructor
     public GameInfoController(GameInfo gameInfo) {
         this.gameInfo = gameInfo;
     }
+
+    /**
+     * to get the action performed and create a new game frame based on the action performed
+     * @param actionEvent event for the action
+     */
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         if(actionEvent.getSource() == gameInfo.getBackButton()) {
