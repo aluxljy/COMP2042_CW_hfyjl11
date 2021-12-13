9bcbc98eaa9d98decdcb12d7f3975daab16653e3
aluxljy <aluxljy@gmail.com>
2021-12-13 16:39:23 +0800

    completed the permanent high score list functionality using JavaFX, added javadocs for GameHighScore, GameHighScoreFrame and GameHighScoreController class

--- src/BrickDestroyer/Controller/GameHighScoreController.java
+++ src/BrickDestroyer/Controller/GameHighScoreController.java
@@ -1,23 +1,30 @@
 package BrickDestroyer.Controller;
 
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.ArrayList;
 
 import BrickDestroyer.Model.GameHighScore;
+import BrickDestroyer.View.GameFrame;
 import org.json.JSONObject;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+/**
+ * Controller for GameHighScoreFrame
+ */
 public class GameHighScoreController {
+    private GameFrame gameFrame;
 
     @FXML
     private Button btnBack;
 
     @FXML
     private Label lblHighScoreTitle;
+
+    @FXML
+    private Label lblPlayer;
 
     @FXML
     private Label lblPlayer_1;
@@ -38,6 +45,9 @@ public class GameHighScoreController {
     private Label lblPlayer_6;
 
     @FXML
+    private Label lblScore;
+
+    @FXML
     private Label lblScore_1;
 
     @FXML
@@ -56,24 +66,76 @@ public class GameHighScoreController {
     private Label lblScore_6;
 
     @FXML
-    public void initialize() throws IOException, ParseException{
+    private Label lblTime;
 
-        ArrayList<JSONObject> highScores = new ArrayList<>();
+    @FXML
+    private Label lblTime_1;
 
+    @FXML
+    private Label lblTime_2;
+
+    @FXML
+    private Label lblTime_3;
+
+    @FXML
+    private Label lblTime_4;
+
+    @FXML
+    private Label lblTime_5;
+
+    @FXML
+    private Label lblTime_6;
+
+    /**
+     * to create a new game frame based on the button clicked
+     */
+    @FXML
+    void btnBackClicked() {
+        gameFrame.getGameHighScoreFrame().dispose();
+        gameFrame.dispose();
+        new GameFrame().initialize();
+    }
+
+    /**
+     * to initialize game high score frame
+     * @throws IOException compulsory when involving IO
+     */
+    @FXML
+    public void initialize() throws IOException {
+        ArrayList<JSONObject> highScores;
         highScores = GameHighScore.PlayerList();
 
+        btnBack.setText("BACK TO MENU");
+        lblHighScoreTitle.setText("HIGH SCORES");
+        lblPlayer.setText("NAME");
+        lblScore.setText("SCORE");
+        lblTime.setText("TIME");
         lblPlayer_1.setText((String) highScores.get(0).get("Name"));
         lblScore_1.setText((String) highScores.get(0).get("Score"));
+        lblTime_1.setText((String) highScores.get(0).get("Time"));
         lblPlayer_2.setText((String) highScores.get(1).get("Name"));
         lblScore_2.setText((String) highScores.get(1).get("Score"));
+        lblTime_2.setText((String) highScores.get(1).get("Time"));
         lblPlayer_3.setText((String) highScores.get(2).get("Name"));
         lblScore_3.setText((String) highScores.get(2).get("Score"));
+        lblTime_3.setText((String) highScores.get(2).get("Time"));
         lblPlayer_4.setText((String) highScores.get(3).get("Name"));
         lblScore_4.setText((String) highScores.get(3).get("Score"));
+        lblTime_4.setText((String) highScores.get(3).get("Time"));
         lblPlayer_5.setText((String) highScores.get(4).get("Name"));
         lblScore_5.setText((String) highScores.get(4).get("Score"));
+        lblTime_5.setText((String) highScores.get(4).get("Time"));
         lblPlayer_6.setText((String) highScores.get(5).get("Name"));
         lblScore_6.setText((String) highScores.get(5).get("Score"));
+        lblTime_6.setText((String) highScores.get(5).get("Time"));
+    }
+
+    /**
+     * called in GameHighScoreFrame, setter
+     * @param gameFrame current game frame
+     */
+    public void setGameFrame(GameFrame gameFrame) {
+        this.gameFrame = gameFrame;
     }
 }
 
--- src/BrickDestroyer/Model/GameHighScore.java
+++ src/BrickDestroyer/Model/GameHighScore.java
@@ -11,7 +11,15 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+/**
+ * to handle the high scores of the game
+ */
 public class GameHighScore {
+    /**
+     * to read the data in the json file to an array list
+     * @return ranking array list
+     * @throws IOException compulsory when involving IO
+     */
     public static ArrayList<JSONObject> PlayerList() throws IOException {
         FileReader file = new FileReader("src/BrickDestroyer/Model/Highscores.json");
         JSONTokener token = new JSONTokener(file);
@@ -36,23 +44,45 @@ public class GameHighScore {
         return ranking;
     }
 
-    // to check if the score is a top 6 high score
-    public static boolean Check(int score) throws IOException {
+    /**
+     * to check if the score made it into the top 6 of the high score list
+     * @param score score obtained by the player
+     * @param time time remaining for the player to complete all levels
+     * @return true or false
+     * @throws IOException compulsory when involving IO
+     */
+    public static boolean Check(int score, String time) throws IOException {
         ArrayList<JSONObject> playerList = PlayerList();
+        String[] timeString = time.split(":");
+
+        int seconds = Integer.parseInt(timeString[0]) * 60 + Integer.parseInt(timeString[1]);
 
         for(Object player: playerList){
             JSONObject jsonPlayer = (JSONObject) player;
-            if (score > Integer.parseInt((String) jsonPlayer.get("Score"))) {
+            String[] previousTimeString = ((String)jsonPlayer.get("Time")).split(":");
+            int previousSeconds = Integer.parseInt(previousTimeString[0]) * 60 + Integer.parseInt((previousTimeString[1]));
+            if(score > Integer.parseInt((String) jsonPlayer.get("Score"))) {
+                return true;
+            }
+            else if(score == Integer.parseInt((String) jsonPlayer.get("Score")) && seconds > previousSeconds) {
                 return true;
             }
         }
         return false;
     }
 
-    public static void AddPlayer(String playerName, Integer highScore) throws IOException {
+    /**
+     * to add a new player that made into the top 6 into the high score list
+     * @param playerName name of the player
+     * @param highScore high score obtained by the player
+     * @param time time remaining for the player to complete all levels
+     * @throws IOException compulsory when involving IO
+     */
+    public static void AddPlayer(String playerName, Integer highScore, String time) throws IOException {
         JSONObject newPlayer = new JSONObject();
         newPlayer.put("Name",playerName);
         newPlayer.put("Score",highScore.toString());
+        newPlayer.put("Time",time);
 
         JSONArray newHighScores = new JSONArray();
 
@@ -60,7 +90,15 @@ public class GameHighScore {
 
         boolean added = false;
 
+        String[] timeString = time.split(":");
+
+        int seconds = Integer.parseInt(timeString[0]) * 60 + Integer.parseInt(timeString[1]);
+        int n = 0;
+
         for (int i = 0; i < 6; i++) {
+            String[] previousTimeString = ((String)playerList.get(i).get("Time")).split(":");
+            int previousSeconds = Integer.parseInt(previousTimeString[0]) * 60 + Integer.parseInt((previousTimeString[1]));
+
             if (!added && (Integer.parseInt((String) newPlayer.get("Score")) > Integer.parseInt((String) playerList.get(i).get("Score")))) {
                 newHighScores.put(i, newPlayer);
                 added = true;
@@ -69,8 +107,24 @@ public class GameHighScore {
                 if (i >= 6 ) {
                     break;
                 }
+                newHighScores.put(i, playerList.get(n));
+                n++;
             }
-            newHighScores.put(i, playerList.get(i));
+            else if(!added && (Integer.parseInt((String) newPlayer.get("Score")) == Integer.parseInt((String) playerList.get(i).get("Score"))) && seconds > previousSeconds) {
+                newHighScores.put(i, newPlayer);
+                added = true;
+                i++;
+
+                if (i >= 6 ) {
+                    break;
+                }
+                newHighScores.put(i, playerList.get(n));
+                n++;
+            }
+            else {
+                newHighScores.put(i, playerList.get(n));
+                n++;
+            }
         }
 
         try (FileWriter file = new FileWriter("src/BrickDestroyer/Model/Highscores.json")) {
